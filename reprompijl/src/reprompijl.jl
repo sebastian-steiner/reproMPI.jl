ee754ba40641530881bb3bc0d45d596cf2f3635b
Sebastian Steiner <e11777731@student.tuwien.ac.at>
2020-04-18 18:24:22 +0200

    Added verbose time output

--- reprompijl/src/reprompijl.jl
+++ reprompijl/src/reprompijl.jl
@@ -14,7 +14,10 @@ struct Args
     calls::Array{Collective,1}
     sizes::Array{Int64,1}
     operation::MPI.Op
+    verbose::Bool
 end
+
+const root = 0
 
 function str_to_collective(str::SubString{String})::Collective
     if str == "MPI_Allreduce"
@@ -41,12 +44,16 @@ function parse_parameters()::Args
             help = "set number of experiment repetitions"
             arg_type = Int
             default = 10
+        "--verbose", "-v"
+            help = "increase verbosity level (print times measured for each process)"
+            action = :store_true
     end
     args = parse_args(ARGS, s)
     nrep = args["nrep"]
     calls = [str_to_collective(ss) for ss in  split(args["calls-list"], ",")]
     message_sizes = [parse(Int, ss) for ss in split(args["msizes-list"], ",")]
-    Args(nrep, calls, message_sizes, MPI.BOR)
+    verbose = args["verbose"]
+    Args(nrep, calls, message_sizes, MPI.BOR, verbose)
 end
 
 function print_info(args::Args)
@@ -63,12 +70,38 @@ function print_info(args::Args)
     println("#@nrep=", args.nrep)
     println("#@root_proc=", 0)
     println("#@nprocs=", MPI.Comm_size(MPI.COMM_WORLD))
+    println("#@verbose=", args.verbose)
 end
 
-function print_results(times::Array{Float64,1}, args::Args, call::Collective, size::Int64)
+function print_verbose(times::Array{Float64,1}, args::Args, call::Collective, msize::Int64)
     comm = MPI.COMM_WORLD
     rank = MPI.Comm_rank(comm)
-    root = 0
+    size = MPI.Comm_size(comm)
+
+    # alloc space for times
+    if rank == root
+        all_times = Array{Float64,2}(undef, args.nrep, size)
+    else
+        all_times = nothing
+    end
+
+    # get times from other processes
+    MPI.Gather!(times, all_times, args.nrep, root, comm)
+
+    # print times
+    if rank == root
+        for proc_id in 1:size
+            for i in 1:args.nrep
+                Printf.@printf("%7d %50s %10d %12ld %14.10f\n", proc_id,
+                    call, i, msize, all_times[i, proc_id])
+            end
+        end
+    end
+end
+
+function print_simple(times::Array{Float64,1}, args::Args, call::Collective, size::Int64)
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
 
     # calculate max runtimes
     maxRuntimes = MPI.Reduce(times, max, root, comm)
@@ -76,8 +109,16 @@ function print_results(times::Array{Float64,1}, args::Args, call::Collective, si
     # print results
     if rank == root
         for i in 1:args.nrep
-            Printf.@printf "%50s %10d %12ld %14.10f\n" call i size maxRuntimes[i]
+            Printf.@printf("%50s %10d %12ld %14.10f\n", call, i, size, maxRuntimes[i])
         end
+    end
+end
+
+function print_results(times::Array{Float64,1}, args::Args, call::Collective, size::Int64)
+    if args.verbose
+        print_verbose(times, args, call, size)
+    else
+        print_simple(times, args, call, size)
     end
 end
 
@@ -86,28 +127,18 @@ function bench(args::Args)
     rank = MPI.Comm_rank(comm)
     size = MPI.Comm_size(comm)
     times = zeros(Float64, args.nrep)
-    root = 0
 
+    # print header
     if rank == root
-        Printf.@printf "%50s %10s %12s %14s\n" "test" "nrep" "count" "runtime_sec"
+        if args.verbose
+            Printf.@printf("%7s %50s %10s %12s %14s\n",  "process", "test", "nrep", "count", "time")
+        else
+            Printf.@printf("%50s %10s %12s %14s\n", "test", "nrep", "count", "runtime_sec")
+        end
     end
 
     for msize in args.sizes
         for call in args.calls
-            #collective_call() = println("ERROR COLLECTIVE CALL NOT SET")
-            #if call == MPI_Allreduce
-            #    send = zeros(UInt8, msize)
-            #    recv = zeros(UInt8, msize)
-            #    collective_call() = MPI.Allreduce!(send, recv, root, args.operation, comm)
-            #elseif call == MPI_Alltoall
-            #    send = zeros(UInt8, msize * size)
-            #    recv = zeros(UInt8, msize * size)
-            #    collective_call() = MPI.Alltoall!(send, recv, root, comm)
-            #elseif call == MPI_Bcast
-            #    buf = zeros(UInt8, msize)
-            #    collective_call() = MPI.Bcast!(buf, msize, root, comm)
-            #end
-
             # init sync
             MPI.Barrier(comm)
 
